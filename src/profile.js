540be96b6797f730ccdc602e241fceee29f5e7e9
Sai Krishna <ivnsainitin@gmail.com>
2025-10-26 16:53:48 +0530

    changes in profile

--- src/profile.js
+++ src/profile.js
@@ -3,20 +3,19 @@ import CustomCamera from './components/camera';
 import { SCENE } from './components/constants';
 import { LuminosityHighPassShader, OrbitControls } from 'three/examples/jsm/Addons.js';
 import { POSTEXTURE, PWALLTEXTURE, repeatTextures, WALLTEXTURE} from './components/textures';
-import { normalMap } from 'three/tsl';
 
-
+let poster;
 
 const customCamera = new CustomCamera();
 const camera = customCamera.createCamera();
-camera.position.set(0,-0.3,3)
+camera.position.set(0,0,3)
 const scene = new THREE.Scene();
 
 
 _setUpFloor();
-_setUpWalls();
-_setUpLights();
 _setUpPoster();
+_setUpWalls();
+
 
 function _setUpFloor(){
     const floorGeometry = new THREE.BoxGeometry(18,5,1);
@@ -26,7 +25,7 @@ function _setUpFloor(){
 
     const floor = new THREE.Mesh(floorGeometry, floorMaterial);
     floor.rotation.x = -(Math.PI / 2);
-    floor.position.y = -3;
+    floor.position.y = -4;
 
     floor.name = "floor"
 
@@ -44,10 +43,9 @@ function _setUpWalls(){
         aoMap: PWALLTEXTURE.AO,
         //normalMap: PWALLTEXTURE.NORMAL,
         displacementMap: PWALLTEXTURE.HEIGHT,
-        metallicMap: PWALLTEXTURE.METALLIC,
+        metalnessMap: PWALLTEXTURE.METALLIC,
         roughness: 0.6,
         metalness: 0.0,
-        clearcoat: 0.2,
         clearcoatRoughness: 0.1
     });
 
@@ -55,58 +53,77 @@ function _setUpWalls(){
     repeatTextures(repeatArr,5,1);
 
     const wall = new THREE.Mesh(wallGeometry,wallMaterial);
-    wall.castShadow = true;
-    wall.position.set(0,1,-2);
+    wall.receiveShadow = true;
+    wall.position.set(0,0,-2);
     scene.add(wall);
 }
 
-function _setUpLights(){
-    const ambientLight = new THREE.AmbientLight(0x101010, 10);
+function _setUpLights() {
+    const ambientLight = new THREE.AmbientLight(0x101010, 15);
     scene.add(ambientLight);
 
-    // const hangingLight = new THREE.SpotLight(0xffffff,10);
-    // hangingLight.position.set(0,5,-1);
-    // hangingLight.target.position.set(0,0,-2);
-    // hangingLight.angle = Math.PI;
-    // hangingLight.penumbra = 0.6;
-    // hangingLight.intensity = 35;
-    // hangingLight.distance = 35;
-    // hangingLight.shadow.bias = -0.01;
-    // hangingLight.castShadow = true;
+    const hangingLight = new THREE.SpotLight(0xffffff, 50);
+    hangingLight.position.set(0, 4.5, -1);
+    hangingLight.target.position.set(0, 0, -1.4);
+    hangingLight.angle = Math.PI / 5;
+    hangingLight.penumbra = 0.3;
+    hangingLight.decay = 2.0;
+    hangingLight.distance = 25;
 
-    // hangingLight.shadow.mapSize.set(2048,2048);
-    // hangingLight.shadow.bias = -0.01;
+    hangingLight.castShadow = true;
+    hangingLight.shadow.mapSize.set(4096, 4096);
+    hangingLight.shadow.bias = -0.0001;
+    hangingLight.shadow.normalBias = 0.02;
 
-    // scene.add(hangingLight);
-    // scene.add(hangingLight.target);
+    // // Adjust shadow camera
+    // hangingLight.shadow.camera.near = 1;
+    // hangingLight.shadow.camera.far = 15;
 
-    const light = new THREE.SpotLight(0xFFFFFF, 75);
-    light.penumbra = 0.1;
-    light.decay = 2.7;
-    light.angle = Math.PI / 10;
+    scene.add(hangingLight);
+    scene.add(hangingLight.target);
 
-    light.position.set(0,-3,3);
-    light.castShadow = true;
-    light.target.position.set(-1,-1.5,-1.4);
-    scene.add(light);
-    scene.add(light.target);
+
+
+
+    const ldlight = new THREE.SpotLight(0xFFFFFF, 65);
+    // ldlight.penumbra = 0.1;
+    // ldlight.decay = 2.7;
+    // ldlight.angle = Math.PI / 10;
+
+    // ldlight.position.set(0,-3,3);
+    // ldlight.castShadow = true;
+    // ldlight.target.position.set(-1,-1.5,-1.4);
+
+    // const ltlight = ldlight.clone();
+    // ltlight.position.set(0,3,3);
+    // ltlight.target.position.set(-1,1.5,-1.4);
+
+    // scene.add(ltlight);
+    // scene.add(ltlight.target);
+
+
+    // scene.add(ldlight);
+    // scene.add(ldlight.target);
 }
 
+
 function _setUpPoster(){
-    const posterGeometry = new THREE.PlaneGeometry(4,5);
+    const posterGeometry = new THREE.BoxGeometry(5,5.5,0.05);
     const posterMaterial = new THREE.MeshStandardMaterial({
         map: POSTEXTURE,
         transparent: true,
-        alphaTest: 0.45,      // removes fully-transparent pixels and helps with sorting
-        roughness: 0.9,
+        //alphaTest: 0.45,
+        roughness: 0.6,
         metalness: 0.0,
     });
 
-    const poster = new THREE.Mesh(posterGeometry,posterMaterial);
+    poster = new THREE.Mesh(posterGeometry,posterMaterial);
     poster.position.set(0,0,-1.4);
     poster.castShadow = true;
 
     scene.add(poster);
+    
+_setUpLights();
 }
 
 
@@ -117,6 +134,8 @@ export function startProfileLoop(renderer){
     renderer.toneMappingExposure = 1.1;
     renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap; // smoother shadow edges
+
 
     renderer.setAnimationLoop(() => {
         renderer.render(scene,camera)
